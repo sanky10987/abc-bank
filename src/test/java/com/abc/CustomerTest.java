ea4fd56a0842e9c41fe81f87c41e69d60e31fa2c
sanket kulkarni <sanky10987@gmail.com>
2015-08-06 07:36:33 -0400

    Added tests for transfer between accounts

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -56,6 +56,36 @@ public class CustomerTest {
 		assertEquals(3, oscar.getNumberOfAccounts());
 	}
 
+	@Test
+	public void testTransferBetweenCheckingAndSavingsAccounts() {
+		Account checkingAccount = new Account(Account.CHECKING);
+		Account savingsAccount = new Account(Account.SAVINGS);
+		oscar.openAccount(checkingAccount);
+		oscar.openAccount(savingsAccount);
+
+		savingsAccount.deposit(4000.0);
+		oscar.transferBetweenAccounts(savingsAccount, checkingAccount, 500);
+		
+		assertEquals(3500.0, savingsAccount.sumTransactions(), 2);
+		
+		assertEquals(500.0, checkingAccount.sumTransactions(), 2);
+	}
+	
+	@Test
+	public void testTransferBetweenSavingsAndMaxiAccounts() {
+		Account savingsAccount = new Account(Account.SAVINGS);
+		Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
+		oscar.openAccount(maxiSavingsAccount);
+		oscar.openAccount(savingsAccount);
+
+		savingsAccount.deposit(4000.0);
+		oscar.transferBetweenAccounts(savingsAccount, maxiSavingsAccount, 500);
+		
+		assertEquals(3500.0, savingsAccount.sumTransactions(), 2);
+		
+		assertEquals(500.0, maxiSavingsAccount.sumTransactions(), 2);
+	}
+
 	@After
 	public void tearDown() {
 		oscar = null;
